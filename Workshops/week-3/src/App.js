6fcfb025caa6b35dc3fa919469c5865520646bd4
shunlaeoo <shunlaeoo98@gmail.com>
2025-04-14 17:36:47 +0630

    week-3

--- Workshops/week-3/src/App.js
+++ Workshops/week-3/src/App.js
@@ -1,11 +1,16 @@
+import Bio from './Bio.js';
 import Gallery from './Gallery.js';
 import { Profile } from './Profile.js';
+import TodoList from './TodoList.js';
 
 export default function App() {
   return (
     <div>
       <Profile />
       <Gallery />
+
+      <TodoList />
+      <Bio />
     </div>
   );
 }
