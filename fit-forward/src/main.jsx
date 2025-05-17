4b56e668de451586acdc42c57610c6d1587008ed
Shun <shunlaeoo98@gmail.com>
2025-05-17 12:42:19 +0630

    route

--- fit-forward/src/main.jsx
+++ fit-forward/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Web_Technologies_ShunLaeOo/fit-forward">
+    <BrowserRouter basename="/Web_Technologies_ShunLaeOo/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
